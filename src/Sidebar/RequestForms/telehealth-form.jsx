6ba4a1e08fd5160d41ab9f146415ca22f5a8e6a4
Chiunmi <jeninecarpio622@gmail.com>
2024-09-26 11:41:01 +0800

    telemed edited

--- src/Sidebar/RequestForms/telehealth-form.jsx
+++ src/Sidebar/RequestForms/telehealth-form.jsx
@@ -18,9 +18,48 @@ function TelehealthAppointmentRequestForm() {
         consequences and may impact your credibility and trustworthiness.
       </p>
       <h3>Telehealth Appointment Request Form </h3>
+      <h4> Available Consultations</h4>
+      <div className="telemed-choices">
+        <div className="telemed-container">
+          <p className="telemed-title"> Dental Consultation</p>
+          <div className="call-button">
+            <p>
+              Connect with a dental professional for telemedicine services
+              related to dental care and queries
+            </p>
+          </div>
+        </div>
+
+        <div className="telemed-container">
+          <p className="telemed-title"> Nursing Consultation</p>
+          <div className="call-button">
+            <p>
+              Interact with nursing professionals for telemedicine services such
+              as patient care guidance and health educationo interact with
+              nursing professionals for telemedicine services such as patient
+              care guidance and health education.
+            </p>
+          </div>
+        </div>
+        <div className="telemed-container">
+          <p className="telemed-title"> Medical Consultation</p>
+          <div className="call-button">
+            <p>
+              Access telemedicine services provided by medical professionals,
+              including general healthcare and medical advice.
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="form-content">
         <label for="student-id">Student ID:</label>
         <input type="text" id="student-id" required />
+        <label for="telehealth-type">Telehealth Consultaion:</label>
+        <select id="telehealth-type" required>
+          <option value="dental-consultation">Dental Consultation</option>
+          <option value="nursing-consultation">Nursing Consultation</option>
+          <option value="medical-consultation">Medical Consultation</option>
+        </select>
 
         <label for="telehealth-date">
           Preferred Telehealth Appointment Date:
@@ -31,11 +70,6 @@ function TelehealthAppointmentRequestForm() {
           Preferred Telehealth Appointment Time:
         </label>
         <input type="time" id="telehealth-time" required />
-
-        <label for="platform">
-          Preferred Telehealth Platform (Zoom, Teams, etc.):
-        </label>
-        <input type="text" id="platform" required />
 
         <label for="reason">Reason for Telehealth Appointment:</label>
         <textarea
--- src/Sidebar/telemed.jsx
+++ src/Sidebar/telemed.jsx
@@ -2,74 +2,10 @@ import "./telemed.css";
 function Telemed() {
   return (
     <div className="telemed-page">
-      <span> To join various telemedicine consultations:</span>
-
-      <div className="telemed-container">
-        <div className="link">
-          <p className="telemed-title"> Dental Consultation</p>
-        </div>
-        <div className="call-button">
-          <h3>
-            Join the call to connect with a dental professional for telemedicine
-            services related to dental care and queries
-          </h3>
-          <button
-            className="join-call-btn"
-            onClick={() => {
-              window.open("https://meet.google.com/xdb-naxn-ewa", "_blank");
-            }}
-          >
-            Join Call
-          </button>
-        </div>
+      <span> View your Telemedicine appointments here.</span>
+      <div className="telemed-appointments">
+        <p> Nothing to see here.</p>
       </div>
-
-      <div className="telemed-container">
-        <div className="link">
-          <p className="telemed-title"> Medical Consultation</p>
-        </div>
-        <div className="call-button">
-          <h3>
-            Join the call to access telemedicine services provided by medical
-            professionals, including general healthcare and medical advice.
-          </h3>
-          <button
-            className="join-call-btn"
-            onClick={() => {
-              window.open("https://meet.google.com/xdb-naxn-ewa", "_blank");
-            }}
-          >
-            Join Call
-          </button>
-        </div>
-      </div>
-
-      <div className="telemed-container">
-        <div className="link">
-          <p className="telemed-title"> Nursing Consultation</p>
-        </div>
-        <div className="call-button">
-          <h3>
-            Join the call to interact with nursing professionals for
-            telemedicine services such as patient care guidance and health
-            educationo interact with nursing professionals for telemedicine
-            services such as patient care guidance and health education
-          </h3>
-          <button
-            className="join-call-btn"
-            onClick={() => {
-              window.open("https://meet.google.com/xdb-naxn-ewa", "_blank");
-            }}
-          >
-            Join Call
-          </button>
-        </div>
-      </div>
-
-      <p className="trouble">
-        {" "}
-        Having Trouble? <a href="#">Click here.</a>
-      </p>
     </div>
   );
 }
